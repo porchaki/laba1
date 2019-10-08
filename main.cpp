46a062e0b66a9edf22b791a8133c126b2ea6c0e3
porchaki <55391902+porchaki@users.noreply.github.com>
2019-10-08 19:21:25 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r, S;
+	cout<<"Enter radius"<<endl;
+	cin>>r;
+	S = r*r*3.14;
+	cout<<"S ="<<S<<endl;
+	system("pause");
+	return 0;
+}
